3ef0a6c5d4c6ad02aca6bbaa93c2b0a05c4700d3
unknown <madhinabanu7866@gmail.com>
2024-10-21 08:14:00 -0700

    Added Js

---
+++ index.js
@@ -0,0 +1,19 @@
+//Selecting Side Navbar,Menu icon
+
+var sidenav = document.getElementById("sidenav")
+var menuicon = document.getElementById("menuicon")
+var closenav = document.getElementById("close-nav")
+
+
+menuicon.addEventListener("click",function(){
+    sidenav.style.right=0
+})
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right = "-50%"
+})
+
+
+
+
+
